123b83765a13f5b922909ba742e5ebfe4dcab0e2
Shamna2005 <shamnasherikp@gmail.com>
2025-08-22 11:11:21 +0530

    Add files via upload

---
+++ primenumber.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+int i,n,prime=1;
+printf("enter a number");
+scanf("%d",&n);
+if(n<=1)
+prime=0;
+else{
+for(i=2;i*i<=n;i++)
+{
+if(n%i==0)
+{
+prime=0;
+break;
+}
+}
+}
+if(prime==1)
+{
+printf("The number %d is a prime number",n);
+}
+else
+{
+printf("The number %d is not a prime number",n);
+}
+return 0;
+}
+
+
